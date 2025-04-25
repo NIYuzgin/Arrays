6c2208efc9d421bcc6970492622881748fc633c0
Николай Немов <prost91@outlook.com>
2025-04-25 17:52:17 +0300

    Repeats encoding

---
+++ Repeats/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+void main() {
+	setlocale(LC_ALL, "");
+
+	/*
+		4. Есть массив из десяти элементов, заполненный случайными числами от 0 до 5.
+   Нужно найти повторяющиеся значения в этом массиве, вывести их на экран,
+   и для каждого повторяющегося значения вывести на экран количество повторений.
+	*/
+
+	srand(time(NULL));
+	const int n = 10;
+	int arr[n];
+	int minRand = 0, maxRand = 6;
+	int repet = 0, num;
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+	cout << endl << "Исходный массив:" << endl << endl;
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << "\t";
+	}
+	cout << endl << endl << "Повторяющиеся значения:" << endl << endl;
+
+	for (int i = 0; i < n; i++) {
+		repet = 0;
+		num = arr[i];
+
+		for (int j = i + 1; j < n; j++) {
+			if (arr[i] == arr[j]) {
+				repet++;
+				arr[j] = -1;
+			}
+		}
+		if ((repet > 0) && (num > 0)) cout << "\t" << num << "\t" << "повторений: " << repet << "\t" << endl;
+	}
+}
---
+++ Unique/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+void main() {
+	setlocale(LC_ALL, "");
+
+	/*
+		3. Массив из десяти элементов заполнить уникальными случайными числами 
+		   в диапазоне от 70 до 80
+	*/
+	
+	srand(time(NULL));
+	const int n = 10;
+	int arr[n];
+	int minRand = 70, maxRand = 81;
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << "\t";
+	}
+	cout << endl << endl;
+}
